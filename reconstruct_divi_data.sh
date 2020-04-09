7039554c4e52f7ab79505248622250647ad6fdd9
Peter Schwede <pschwede@posteo.de>
2020-04-09 20:17:06 +0200

    upd: newest data

--- reconstruct_divi_data.sh
+++ reconstruct_divi_data.sh
@@ -9,8 +9,42 @@ paste data/divi.tsv <(
 	echo "commit_date";
 
 	# extract datetime from git blame
-	git blame data/divi.tsv \
+	git blame d7c57a2032b4ef79d7fbec83d7938e1999c001ee^^^ -- data/divi.tsv \
 		| awk -F$' ' 'NR>1{print $3" "$4}'
 ) \
 	| awk -F'\t' 'BEGIN{OFS=FS} $9==""{$9=$(10)} {$(10)="";print $0}' \
-	> data/divi.fixed.tsv
+	| awk -F$'\t' 'BEGIN{OFS=FS}{$1=$9;NF=7}1' \
+	> data/divi.fixed.03.tsv
+
+paste data/divi.tsv <(
+	echo "commit_date";
+
+	# extract datetime from git blame
+	git blame d7c57a2032b4ef79d7fbec83d7938e1999c001ee^^ -- data/divi.tsv \
+		| awk -F$' ' 'NR>1{print $3" "$4}'
+) \
+	| awk -F'\t' 'BEGIN{OFS=FS} $9==""{$9=$(10)} {$(10)="";print $0}' \
+	| awk -F$'\t' 'BEGIN{OFS=FS}{$1=$9;NF=7}1' \
+	> data/divi.fixed.02.tsv
+
+paste data/divi.tsv <(
+	echo "commit_date";
+
+	# extract datetime from git blame
+	git blame d7c57a2032b4ef79d7fbec83d7938e1999c001ee^ -- data/divi.tsv \
+		| awk -F$' ' 'NR>1{print $3" "$4}'
+) \
+	| awk -F'\t' 'BEGIN{OFS=FS} $9==""{$9=$(10)} {$(10)="";print $0}' \
+	| awk -F$'\t' 'BEGIN{OFS=FS}{$1=$9;NF=7}1' \
+	> data/divi.fixed.01.tsv
+
+paste data/divi.tsv <(
+	echo "commit_date";
+
+	# extract datetime from git blame
+	git blame d7c57a2032b4ef79d7fbec83d7938e1999c001ee -- data/divi.tsv \
+		| awk -F$' ' 'NR>1{print $3" "$4}'
+) \
+	| awk -F'\t' 'BEGIN{OFS=FS} $9==""{$9=$(10)} {$(10)="";print $0}' \
+	| awk -F$'\t' 'BEGIN{OFS=FS}{$1=$9;NF=7}1' \
+	> data/divi.fixed.00.tsv
