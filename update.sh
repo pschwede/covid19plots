794b1497b826a77dd8239bf8b173f3f16eee8e30
pschwede <pschwede@posteo.de>
2020-10-18 11:55:15 +0200

    upd: data

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 HERE=$(readlink -f $(dirname $0))
 ENVACT="env/bin/activate"
 
-(
+{
 	cd "$HERE"
 
 	if [ -e $ENVACT ]; then
@@ -14,12 +14,17 @@ ENVACT="env/bin/activate"
 
 	scrape/divi.py data/divi.tsv \
 	&& git add data/divi.tsv \
-	&& git commit -m "upd: newest data" \
-	&& git push origin
+	&& git commit -m "upd: newest data"
+
+	./run-notebooks.py \
+	&& git add *.ipynb \
+	&& git commit -m "upd: data"
+
+	git push origin
 
 	if [ -e $ENVACT ]; then
 		deactivate
 	fi
 
 	cd -
-) 1>&2 2>$HERE/update.log
+} 1>&2 2>$HERE/update.log
