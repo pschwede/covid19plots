9b15bebcfdbb7fce6b86a525a838ac39fbafa76c
Peter Schwede <pschwede@posteo.de>
2021-04-28 19:49:03 +0200

    reduced reqs; divi ipynb -> py

---
+++ bin/divi.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from glob import glob
+from datetime import datetime
+import matplotlib.dates as mdates
+
+from scrape import scrape, divi, entorb
+
+federal_state_translation = {
+        "BB": "Brandenburg",
+        "BE": "Berlin",
+        "BW": "Baden-Württemberg",
+        "BY": "Bayern",
+        "HB": "Bremen",
+        "HE": "Hessen",
+        "HH": "Hamburg",
+        "MV": "Mecklenburg-Vorpommern",
+        "NI": "Niedersachsen",
+        "NW": "Nordrhein-Westfalen",
+        "RP": "Rheinland-Pfalz",
+        "SH": "Schleswig-Holstein",
+        "SL": "Saarland",
+        "SN": "Sachsen",
+        "ST": "Sachsen-Anhalt",
+        "TH": "Thüringen",
+        }
+
+def load_tsv():
+    """Load local tsv file"""
+    df = pd \
+            .read_csv("data/divi.tsv", sep="\t") \
+            .replace({
+                'NaN': 0.0,
+                'VERFUEGBAR': 1.0,
+                'BEGRENZT': 0.5,
+                'NICHT_VERFUEGBAR': 0.0
+                })
+    df = df.drop_duplicates()
+    df['Stand'] = pd.to_datetime(df['Stand'], format="%Y-%m-%d %H:%M:%S")
+    return df
+
+
+def plot_german_hbars(df, fname):
+    """Plot a horizontal bar for each federal state."""
+    df = df[(df['Stand'] > '2019-12-31') & (df['Stand'] <= datetime.now())].set_index('Stand') \
+            .drop(columns='COVID-19 cases')
+
+    fig, ax = plt.subplots()
+    df \
+            .groupby('Bundesland').mean().reset_index() \
+            .resample('D', on='Stand') \
+            .plot(kind='barh', ax=ax, x='Bundesland', y=['ECMO', 'ICU high care', 'ICU low care'], stacked=True, figsize=(8,8))
+    ax.set_title("German mean hospital occupancy")
+    ax.set_xticks([0.0, 1.5, 3.0])
+    ax.set_xticklabels(["available", "limited", "exhausted"])
+    ax.set_ylabel("federal state")
+    fig.savefig(fname)
+    
+def plot_history(df, fname):
+    """Plot each federal state's total exhaustion from @entorb but also keep each curve in
+    dict bundesland_curves for later correlations"""
+    columns = ['ICU low care', 'ICU high care', 'ECMO']
+    availability = dict()
+    num_clinics = dict()
+    infections = dict()
+    deaths = dict()
+
+    for federal_state in federal_state_translation:
+        clinics_curves_sum = None
+        federal_state_clinics = list(set(df[df['Bundesland'] == federal_state]['Klinikname']))
+        # apply padding
+        for clinic in federal_state_clinics:
+            clinic_curve = df[df['Klinikname'] == clinic] \
+                    .drop(columns=['Bundesland', 'Klinikname']) \
+                    .bfill()
+            clinics_curves_sum = clinic_curve if clinics_curves_sum is None else clinics_curves_sum.add(clinic_curve, fill_value=0.5)
+            availability[federal_state] = clinics_curves_sum
+            num_clinics[federal_state] = len(federal_state_clinics)
+            #infections[federal_state] = entorb.to_dataframe(federal_state)['Cases_New']
+            deaths[federal_state] = entorb.to_dataframe(federal_state)['Deaths_New']
+            deaths[federal_state].name = 'Deaths'
+
+    for federal_state in sorted(federal_states):
+        fig, ax = plt.subplots()
+
+        ax = deaths[federal_state].plot(kind='line', ax=ax, color='grey', linestyle=':', marker='+', legend=True, figsize=(16,9))
+        ax.set_xlabel('Deaths')
+
+        ax = availability[federal_state].plot(kind='line', ax=ax, secondary_y=True, marker='') # drawstyle=steps-post
+        ax.set_yticks([
+            0.0 * num_clinics[federal_state],
+            0.5 * num_clinics[federal_state],
+            1.0 * num_clinics[federal_state],
+            ])
+        ax.set_ytickslabels(["available", "limited", "unavailable"])
+        ax.set_title("%s" % federal_state_translation[federal_state])
+
+        fig.savefig(fname % {'state': federal_state})
+        return
+
+
+if __name__ == "__main__":
+    df = load_tsv()
+    plot_german_hbars(df, fname="img/hbars.svg")
+    plot_history(df=df, fname="test.svg") 
---
+++ doc/divi.md
@@ -0,0 +1,21 @@
+# DIVI Intensivregister
+
+## What are we dealing with?
+
+[The divi](https://www.intensivregister.de/#/intensivregister) lists many (if not all) German clinics and their status of ICU /ECMO availability.
+
+* green = /Verfügbar/ = available
+* yellow = /Begrenzt/ = limited
+* red = /Ausgelastet/ = exhausted
+* unavailable = /Unbekannt/ = unknown
+
+For plotting, I weighted those three values
+
+* green = 0.0
+* yellow = 0.5
+* red = 1.0
+* unavailable = NaN.
+
+## Plots
+
+
